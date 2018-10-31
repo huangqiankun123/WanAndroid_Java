95484e9b4b3c8941b3eca413b22d6445348f113b
huangqiankun123 <18671166183@163.com>
2018-10-31 10:46:35 +0800

    更新玩玩 28.0.0

--- app/src/main/java/com/qkun/wanandroid_java/MainActivity.java
+++ app/src/main/java/com/qkun/wanandroid_java/MainActivity.java
@@ -2,12 +2,25 @@ package com.qkun.wanandroid_java;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.iv);
+
+    }
+
+    public void loadImage(View view) {
+        String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+        Glide.with(this).load(url).into(mImageView);
     }
 }
